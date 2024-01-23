a60b41fa25c680395a8883b31e0546c504b42465
AbeMugwe <abemwangi17@gmail.com>
2024-01-23 10:03:03 +0300

    day2

---
+++ WEEK1/day2.js
@@ -0,0 +1,15 @@
+// let sexage= prompt("Had sex with what age?")
+// let ageofconsent= 18
+
+// if(sexage>=ageofconsent) {
+//     console.log("Legal sex");  
+// } else {
+//     console.log('Pedophile')
+// }
+
+
+
+
+for (let i = 0; i < cars.length; i++) {
+    text += cars[i] + "<br>";
+  }
